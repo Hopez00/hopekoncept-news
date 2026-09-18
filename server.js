31009f13cbc81cf1c820e8b21b9bdd1abd0bd727
Hopez00 <167991850+Hopez00@users.noreply.github.com>
2026-09-18 10:40:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.get('/', async (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="monetag" content="bcc8a66d8f9be3076900eb18f6e13e49">
       <title>Hopekoncept News - Global & Local Feed</title>
       <style>
         body { font-family: Helvetica, Arial, sans-serif; margin: 0; background-color: #f1f5f9; color: #0f172a; position: relative; min-height: 100vh; }
